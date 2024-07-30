73806371830533faa5bf88abb28d975704bf587a
SaswatSambit <saswat.ceh@gmail.com>
2024-07-30 10:06:30 +0530

    Update wsgi.py

--- wsg.py
+++ wsg.py
@@ -1,4 +1,6 @@
-from app import app as application
+import sys
+import logging
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0, "/home/Ubuntu/subdomain_scanner")
 
-if __name__ == "__main__":
-    application.run()
+from app import app as application
